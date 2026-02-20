dafabb106fc2561d4915b80f5ae4bae3850eb9c4
PirraToZ <pirratoz@gmail.com>
2026-02-20 03:52:16 +0300

    fix(): module with sql files

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="migrations",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
     install_requires=install_requires,
     author="Trushin Pavel",
